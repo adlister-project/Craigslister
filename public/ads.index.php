5115658b49e77f381f151de31dd5b3e4b04ada09
Craig Re <car.code240z@gmail.com>
2015-12-14 15:57:19 -0600

    Modified the footer file and the ads index file.

---
+++ public/ads.index.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/uikit.almost-flat.min.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <title>ads index</title>
+    </head>
+    <body>
+        <script type="text/javascript" src="js/uikit.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+        <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        </ul>
+    </body>
+    </html>
---
+++ views/partials/footer.php
@@ -0,0 +1,24 @@
+<footer>
+    <p>Posted by: Hege Refsnes</p>
+    <p>Contact information: <a href="mailto:someone@example.com">
+    someone@example.com</a>.</p>
+
+
+
+</footer>
+
+<div class="uk-container uk-container-center uk-text-center">
+
+                <ul class="uk-subnav uk-subnav-line uk-flex-center">
+                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">Home Page</a></li>
+                    <li><a href="#">Log In</a></li>
+                    <li><a href="">Twitter</a></li>
+                </ul>
+
+                <div class="uk-panel">
+                    <p>Made by <a href="http://www.yootheme.com">YOOtheme</a> with love and caffeine.<br>Licensed under <a href="http://opensource.org/licenses/MIT">MIT license</a>.</p>
+                    <a href="../index.html"><img src="images/logo_uikit.svg" width="90" height="30" title="UIkit" alt="UIkit"></a>
+                </div>
+
+            </div>
