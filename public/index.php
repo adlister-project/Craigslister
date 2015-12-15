24fd30dc89a3b35f0c1d66032ef9c2c791878c64
Jonathan R <jreyes.satx@gmail.com>
2015-12-15 09:19:25 -0600

    updated file structure and updated main index head with js script tags

--- public/index.php
+++ public/index.php
@@ -6,14 +6,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Craigslister</title>
         <link rel="stylesheet" href="css/uikit.almost-flat.min.css">
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/uikit.min.js"></script>
         <link rel="stylesheet" href="css/main.css">
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
         <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
     </head>
     <body>
 
+        <div class="uk-container-center">
+            
+        </div>
+
         <?php include '../views/partials/navbar.php' ?>
         <?php include 'js/js_include.php' ?>
     </body>
 </html>
-
