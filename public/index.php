de1df7568efa1f5e43cd0de42c5d9dbbc475462b
Jonathan R <jreyes.satx@gmail.com>
2015-12-15 13:11:29 -0600

    updated index page with changes. still needs more work

--- public/index.php
+++ public/index.php
@@ -13,12 +13,41 @@
         <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
     </head>
     <body>
+        <?php include '../views/partials/navbar.php' ?>
 
         <div class="uk-container-center">
-            
-        </div>
+            <div class="uk-grid data-uk-grid uk-container-center">
+                <div class="uk-width-medium-1-3">
+                    <a class="uk-thumbnail uk-overlay-hover" href="#">
+                        <figure class="uk-overlay">
+                            <img src="http://placehold.it/350x250" alt="300x110" data-src="holder.js/350x250/auto" data-holder-rendered="true">
+                            <div class="uk-overlay-panel uk-overlay-background uk-overlay-bottom uk-overlay-slide-bottom">For SALE!!</div>
+                        </figure>
+                        <div class="uk-thumbnail-caption">List Keyword!!</div>
+                    </a>
+                </div>
+                <div class="uk-width-medium-1-3">
+                    <a class="uk-thumbnail uk-overlay-hover" href="#">
+                        <figure class="uk-overlay">
+                            <img src="http://placehold.it/350x250" alt="300x110" data-src="holder.js/350x250/auto" data-holder-rendered="true">
+                            <div class="uk-overlay-panel uk-overlay-background uk-overlay-bottom uk-overlay-slide-bottom">For SALE!!</div>
+                        </figure>
+                        <div class="uk-thumbnail-caption">List Keyword!!</div>
+                    </a>
+                </div>
+                <div class="uk-width-medium-1-3">
+                    <a class="uk-thumbnail uk-overlay-hover" href="#">
+                        <figure class="uk-overlay">
+                            <img src="http://placehold.it/350x250" alt="300x110" data-src="holder.js/350x250/auto" data-holder-rendered="true">
+                            <div class="uk-overlay-panel uk-overlay-background uk-overlay-bottom uk-overlay-slide-bottom">For SALE!!</div>
+                        </figure>
+                        <div class="uk-thumbnail-caption">List Keyword!!</div>
+                    </a>
+                </div> 
+         </div>
 
-        <?php include '../views/partials/navbar.php' ?>
+         <?php include '../views/partials/footer.php' ?>
+
         <?php include 'js/js_include.php' ?>
     </body>
 </html>
