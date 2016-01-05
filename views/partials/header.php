f4f57452cfae8b11d90afef4f30b0d5d06f6549e
Craig Re <car.code240z@gmail.com>
2016-01-05 10:10:52 -0600

    Added functionality to validation.

---
+++ views/partials/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="img/icon.png">
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
+    <link rel="stylesheet" href="css/uikit.almost-flat.min.css" media="screen">
+    <link rel="stylesheet" href="css/components/form-password.almost-flat.min.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/components/slidenav.almost-flat.min.css">
+    <link rel="stylesheet" type="text/css" href="css/components/slideshow.almost-flat.min.css">
+    <link rel="stylesheet" href="css/main.css" media="screen">
+    <link rel="stylesheet" href="css/create.css" media="screen">
+    <link rel="stylesheet" href="css/adsedit.css">
+    <link rel="stylesheet" type="text/css" href="css/adsindex.css">
+    <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="js/additional-methods.min.js"></script>
+    <script type="text/javascript" src="js/uikit.min.js"></script>
+  </head>
